95fb200d083f2b6d35ffbf8a12d1127faeb3f3d4
Ajinkya Chabukswar (BSL) <ajinkyac@BIRLASOFT.COM>
2022-03-26 11:34:18 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+serv_object = Service("C:\\Users\\ajinkyac\\Downloads\\Drivers\\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_object)
+
+driver.get("https://opensource-demo.orangehrmlive.com/")
+
+driver.find_element(By.ID,"txtUsername").send_keys("Admin")
+driver.find_element(By.ID,"txtPassword").send_keys("admin123")
+driver.find_element(By.CSS_SELECTOR,"input#btnLogin.button").click()
+
+actual_title = driver.title
+expected_title = "OrangeHRM"
+if actual_title==expected_title:
+    print("Title is correct")
+else:
+    print("Title is wrong")
+
+driver.close()
+
+
+
+
+
